bfd0a7670d122816a4355136225213e2edf78d6f
만두_김용태 <giyom83@naver.com>
2023-05-10 22:59:56 +0900

    스크롤 될 때 프로필 영역 줄어들도록 수정.

--- CallMandu/CallMandu/Contact/Detail/ContactDetailView.swift
+++ CallMandu/CallMandu/Contact/Detail/ContactDetailView.swift
@@ -18,139 +18,149 @@ struct ContactDetailView: View {
     @State var data: ContactDatable
     @ScaledMetric private var fontSize: CGFloat = 20
     
+    let profileAreaSize: Double = 150
+    
+    let centerPosition : (GeometryProxy) -> CGPoint = { proxy in
+        return CGPoint(x: proxy.frame(in: .local).midX, y: proxy.frame(in: .local).midY);
+    }
+    
     var body: some View {
-        LazyVStack {
-            LazyVStack {
-                Image(systemName: "person.circle.fill")
-                    .resizable()
-                    .frame(width:100 + fDeltaHeight, height:100 + fDeltaHeight)
-                    .foregroundColor(.green)
-                Text(data.familyName + data.givenName)
-                    .font(.system(size: fontSize))
-                    .foregroundColor(.white)
-                Text(data.jobTitle)
-                    .font(.system(size: 12))
-                    .foregroundColor(.gray)
-            }
-            
-            LazyHStack(spacing:10) {
-                Button {
-                    print("메시지")
-                } label: {
-                    vStack(icon: "message.fill", title: "메시지")
-                }
-                .frame(width: 80, height: 80)
-                .background(Color.darkBg)
-                .disabled(useMsg)
-                .cornerRadius(5)
-                
-                Button {
-                    guard let phoneNumber = data.phoneNumber.first else { return }
-                    let telephone = "tel://"
-                    let formattedString = telephone + phoneNumber
-                    guard let url = URL(string: formattedString) else { return }
-                    UIApplication.shared.open(url)
-                } label: {
-                    vStack(icon: "phone.fill", title: "휴대전화")
-                }
-                .frame(width: 80, height: 80)
-                .background(Color.darkBg)
-                .disabled(useCall)
-                .cornerRadius(5)
-                
-                Button {
-                    print("비디오")
-                } label: {
-                    vStack(icon: "video.fill", title: "비디오")
-                }
-                .frame(width: 80, height: 80)
-                .background(Color.darkBg)
-                .disabled(useVideo)
-                .cornerRadius(5)
-                
-                Button {
-                    print("Mail")
-                } label: {
-                    vStack(icon: "envelope.fill", title: "Mail")
-                }
-                .frame(width: 80, height: 80)
-                .background(Color.darkBg)
-                .disabled(useMail)
-                .cornerRadius(5)
-            }
-            .padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-            
-            ScrollView {
-                LazyVStack {
-                    GroupBox {
-                        LazyVStack(alignment:.leading) {
-                            Text("전화번호")
+        GeometryReader { geomentryReader in
+            VStack(spacing:0) {
+                GeometryReader { profileGeo in
+                        VStack {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .foregroundColor(.green)
+                                .frame(minWidth: 50, maxWidth: 100, minHeight: 50, maxHeight:100)
+
+                                
+                            Text(data.familyName + data.givenName)
+                                .font(.system(size: fontSize))
                                 .foregroundColor(.white)
-                                .font(.system(size: 15))
-                            LazyVStack(alignment: .leading, spacing: 2) {
-                                ForEach(data.phoneNumber, id:\.self) { number in
-                                    Text(number)
-                                        .foregroundColor(.blue)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.5)
+                            
+                            Text(data.jobTitle)
+                                .font(.system(size: 12))
+                                .foregroundColor(.gray)
+                                .minimumScaleFactor(0.5)
+                        }
+                        .position(centerPosition(profileGeo))
+                }
+                .frame(maxWidth: profileAreaSize + fDeltaHeight, maxHeight:profileAreaSize + fDeltaHeight)
+
+                LazyHStack(spacing:10) {
+                    Button {
+                        print("메시지")
+                    } label: {
+                        vStack(icon: "message.fill", title: "메시지")
+                    }
+                    .frame(width: 80, height: 80)
+                    .background(Color.darkBg)
+                    .disabled(useMsg)
+                    .cornerRadius(5)
+                    
+                    Button {
+                        guard let phoneNumber = data.phoneNumber.first else { return }
+                        let telephone = "tel://"
+                        let formattedString = telephone + phoneNumber
+                        guard let url = URL(string: formattedString) else { return }
+                        UIApplication.shared.open(url)
+                    } label: {
+                        vStack(icon: "phone.fill", title: "휴대전화")
+                    }
+                    .frame(width: 80, height: 80)
+                    .background(Color.darkBg)
+                    .disabled(useCall)
+                    .cornerRadius(5)
+                    
+                    Button {
+                        print("비디오")
+                    } label: {
+                        vStack(icon: "video.fill", title: "비디오")
+                    }
+                    .frame(width: 80, height: 80)
+                    .background(Color.darkBg)
+                    .disabled(useVideo)
+                    .cornerRadius(5)
+                    
+                    Button {
+                        print("Mail")
+                    } label: {
+                        vStack(icon: "envelope.fill", title: "Mail")
+                    }
+                    .frame(width: 80, height: 80)
+                    .background(Color.darkBg)
+                    .disabled(useMail)
+                    .cornerRadius(5)
+                }
+                .frame(maxHeight: 80)
+                .padding()
+                
+                ScrollView {
+                    LazyVStack {
+                        GroupBox {
+                            LazyVStack(alignment:.leading) {
+                                Text("전화번호")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 15))
+                                LazyVStack(alignment: .leading, spacing: 2) {
+                                    ForEach(data.phoneNumber, id:\.self) { number in
+                                        Text(number)
+                                            .foregroundColor(.blue)
+                                    }
                                 }
                             }
                         }
-                    }
-                    .background(
-                        GeometryReader{ proxy in
-                            Color.clear
-                                .preference(key: OffsetPreferenceKey.self, value: proxy.frame(in: .named("phoneInfoLayer")).minY )
+                        .background(
+                            GeometryReader{ proxy in
+                                Color.clear
+                                    .preference(key: OffsetPreferenceKey.self, value: proxy.frame(in: .named("phoneInfoLayer")).minY )
+                            }
+                        )
+                        
+                        GroupBox {
+                            LazyVStack(alignment:.leading) {
+                                Text("메모")
+                                    .foregroundColor(.white)
+                                Text("누구게?")
+                                    .foregroundColor(.blue)
+                            }
                         }
-                    )
-                    
-                    GroupBox {
-                        LazyVStack(alignment:.leading) {
-                            Text("메모")
-                                .foregroundColor(.white)
-                            Text("누구게?")
-                                .foregroundColor(.blue)
+                        
+                        GroupBox {
+                            LazyVStack {
+                                onelineTextField("메시지 보내기", underLine: true)
+                                onelineTextField("연락처 공유", underLine: true)
+                                onelineTextField("즐겨찾기에 추가", underLine: true)
+                            }
+                        }
+                        
+                        GroupBox {
+                            onelineTextField("긴급 연락처에 추가")
+                        }
+                        
+                        GroupBox {
+                            onelineTextField("이 발신자 차단", color: .red)
+                        }
+                        
+                        GroupBox {
+                            onelineTextField("목록에 추가")
                         }
                     }
-                    
-                    GroupBox {
-                        LazyVStack {
-                            onelineTextField("메시지 보내기", underLine: true)
-                            onelineTextField("연락처 공유", underLine: true)
-                            onelineTextField("즐겨찾기에 추가", underLine: true)
-                        }
-                    }
-                    
-                    GroupBox {
-                        onelineTextField("긴급 연락처에 추가")
-                    }
-                    
-                    GroupBox {
-                        onelineTextField("이 발신자 차단", color: .red)
-                    }
-                    
-                    GroupBox {
-                        onelineTextField("목록에 추가")
-                    }
-                    GroupBox {
-                        onelineTextField("테스트 항목", color: .yellow)
-                        onelineTextField("테스트 항목", color: .yellow)
-                        onelineTextField("테스트 항목", color: .yellow)
-                        onelineTextField("테스트 항목", color: .yellow)
-                        onelineTextField("테스트 항목", color: .yellow)
-                        onelineTextField("테스트 항목", color: .yellow)
-                    }
-                    
                 }
-            }
-            .frame(minHeight:100, maxHeight: 300)
-            .padding(.zero)
-            .coordinateSpace(name: "phoneInfoLayer")
-            .onPreferenceChange(OffsetPreferenceKey.self) { value in
-                if(value > -50 && value < 0) {
-                    fDeltaHeight = value
+                .frame(minHeight:100, maxHeight: geomentryReader.size.height)
+                .coordinateSpace(name: "phoneInfoLayer")
+                .onPreferenceChange(OffsetPreferenceKey.self) { value in
+                    if(value > -80 && value < 0) {
+                        fDeltaHeight = value
+                        debugPrint(fDeltaHeight)
+                    }
                 }
             }
         }
-        Spacer()
     }
     
     private struct OffsetPreferenceKey: PreferenceKey {
